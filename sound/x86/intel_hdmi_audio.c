d795543e73c1b167a0ff3b4e782f865c87c5296f
Sakari Ailus <sakari.ailus@linux.intel.com>
2024-01-08 16:16:09 +0200

    ALSA: intel_hdmi: Switch to __pm_runtime_put_autosuspend()
    pm_runtime_put_autosuspend() will soon be changed to include a call to
    pm_runtime_mark_last_busy(). This patch switches the current users to
    __pm_runtime_put_autosuspend() which will continue to have the
    functionality of old pm_runtime_put_autosuspend().
    
    Cc: Jaroslav Kysela <perex@perex.cz>
    Cc: Takashi Iwai <tiwai@suse.com>
    Cc: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: linux-sound@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- sound/x86/intel_hdmi_audio.c
+++ sound/x86/intel_hdmi_audio.c
@@ -1102,7 +1102,7 @@ static int had_pcm_open(struct snd_pcm_substream *substream)
 	return retval;
  error:
 	pm_runtime_mark_last_busy(intelhaddata->dev);
-	pm_runtime_put_autosuspend(intelhaddata->dev);
+	__pm_runtime_put_autosuspend(intelhaddata->dev);
 	return retval;
 }
 
@@ -1127,7 +1127,7 @@ static int had_pcm_close(struct snd_pcm_substream *substream)
 	spin_unlock_irq(&intelhaddata->had_spinlock);
 
 	pm_runtime_mark_last_busy(intelhaddata->dev);
-	pm_runtime_put_autosuspend(intelhaddata->dev);
+	__pm_runtime_put_autosuspend(intelhaddata->dev);
 	return 0;
 }
 
@@ -1589,7 +1589,7 @@ static void had_audio_wq(struct work_struct *work)
 
 	mutex_unlock(&ctx->mutex);
 	pm_runtime_mark_last_busy(ctx->dev);
-	pm_runtime_put_autosuspend(ctx->dev);
+	__pm_runtime_put_autosuspend(ctx->dev);
 }
 
 /*
